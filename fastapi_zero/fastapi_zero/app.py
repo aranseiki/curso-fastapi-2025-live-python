6f74ec65d695340d6816b4d297d4c07e9988e9fa
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-05-22 01:22:05 -0300

    Adicionado endpoint de "hello world"

--- fastapi_zero/fastapi_zero/app.py
+++ fastapi_zero/fastapi_zero/app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fastapi_zero.schemas import Message
 
@@ -9,3 +11,24 @@ app = FastAPI(title='FastAPI Zero')
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo!'}
+
+
+@app.get('/hello', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_hello():
+    return """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta
+                name="viewport"
+                content="width=device-width,
+                initial-scale=1.0"
+            >
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+        </html>
+    """

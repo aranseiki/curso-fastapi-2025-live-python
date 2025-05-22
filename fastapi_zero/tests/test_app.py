7b36b4531fabe79e1b079cd35a9857a5850bd677
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-05-22 01:22:20 -0300

    Adicionado testes para endpoint de "hello world"

--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -12,3 +12,28 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.json() == {'message': 'Olá, mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_hello_deve_retornar_html():
+    client = TestClient(app)
+
+    response = client.get('/hello')
+
+    assert response.text == """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta
+                name="viewport"
+                content="width=device-width,
+                initial-scale=1.0"
+            >
+            <title>Hello World</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+        </html>
+    """
+    assert response.status_code == HTTPStatus.OK

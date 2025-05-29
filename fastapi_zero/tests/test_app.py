b301a3fcd278e4de5acaecf0399a622f319288f6
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-05-29 01:46:11 -0300

    Adicionado teste de erros 404 para read_users e delete_user

--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -94,3 +94,24 @@ def test_delete_user(client):
         'email': 'bob@example.com',
         'id': 1,
     }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}

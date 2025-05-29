04398c92c97172ff9583f8597d6917316594f508
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-05-29 02:09:47 -0300

    Adicionado testes para a rota read_user quando for OK ou Not Found

--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -85,6 +85,17 @@ def test_update_user(client):
     }
 
 
+def test_read_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+
+
 def test_delete_user(client):
     response = client.delete('/users/1')
 
@@ -112,6 +123,13 @@ def test_update_user_not_found(client):
 
 def test_delete_user_not_found(client):
     response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found!'}
+
+
+def test_read_user_not_found(client):
+    response = client.get('/users/1')
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'User not found!'}
